f1b48295ed6c58b35da385afebb15581f091ea7f
HamidehMH <mahdiz.hamideh@gmail.com>
2020-10-24 18:55:42 +0330

    #96 Add search book posibility

--- src/components/search-book/index.js
+++ src/components/search-book/index.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 import "./search-book.css";
 
 const SearchBook = ({ name, onSearchClick }) => {
-  const [searchValue, searchValueChange] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [showSearchInput, setShowSearchInput] = useState(false);
+  const className = searchValue ? "search-book-input-selected" : "search-book-container";
   return (
     <form onSubmit={(e) => onSearchClick(e, searchValue)}>
-      <i className="search-book-icon"></i>
-      <input
-        className="search-book-input"
-        name={name}
-        value={searchValue}
-        onChange={(e) => searchValueChange(e.target.value)}
-      />
+      <div className={className} onClick={() => setShowSearchInput(true)}>
+        <i class="material-icons search-book-icon">search</i>
+        {showSearchInput ? (
+          <input
+            className="search-book-input"
+            name={name}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        ) : (
+          ""
+        )}
+      </div>
     </form>
   );
 };
